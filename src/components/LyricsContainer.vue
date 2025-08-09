8cc55fc888c5bc89efc9100dcdfb6e6068889625
Aarón Rodríguez <aaron.532@hotmail.com>
2025-08-08 21:41:44 -0600

    use yt-api

--- src/components/LyricsContainer.vue
+++ src/components/LyricsContainer.vue
@@ -12,7 +12,8 @@ import type {LineLyrics, LyricResult} from "src/plugins/synced-lyrics/types";
 const hasJapanese = ref(false);
 const hasKorean = ref(false);
 const currentTime = ref(0);
-const FETCH_URL = 'http://localhost:1608/';
+// const FETCH_URL = 'http://localhost:1608/';
+const FETCH_URL = 'http://localhost:26538/api/v1/song';
 
 
 const title = ref('');
@@ -22,6 +23,7 @@ const album = ref('');
 const durationMs = ref(0);
 const duration = ref(0);
 const status = ref('');
+const isPaused = ref(false);
 const tags = ref<string[]>([]);
 const fetching = ref('');
 
@@ -51,10 +53,10 @@ function fetch_data() {
     .then(async data => {
       const endTime = performance.now();
       const lag = endTime - startTime;
-      const predictedTime = data['progress'] + lag;
+      const predictedTime = (data['elapsedSeconds'] * 1000) + lag;
       // data now contains the json object with song metadata
       const diffTime = currentTime.value - predictedTime
-      if (Math.abs(diffTime) > 1000) {
+      if (Math.abs(diffTime) > 2000) {
         currentTime.value = predictedTime + 600;
       }
 
@@ -68,19 +70,23 @@ function fetch_data() {
         album.value = data['album'];
         // why in mili? ""  ""      ""                  ""
 
-        durationMs.value = data['duration'];
-        duration.value = data['duration'] / 1000;
+        durationMs.value = data['songDuration'] * 1000;
+        duration.value = data['songDuration'];
         status.value = data['status'];
+        isPaused.value = data['isPaused'];
         tags.value = data['tags'];
 
         fetching.value = 'fetching';
 
+        //en vez de buscar, el ID de la cancion
+        // https://lrclib.net/api/get/3396226
+
         searchResult.value = await LRCLibClass.search({
           title: data['title'],
           alternativeTitle: data['alternativeTitle'],
-          artist: data['artists'][0],
+          artist: data['artist'],
           album: data['album'],
-          songDuration: data['duration'] / 1000,
+          songDuration: data['songDuration'],
           tags: data['tags'],
           videoId: ''
         });
@@ -104,7 +110,7 @@ function fetch_data() {
 
 const requestInteral = setInterval(fetch_data, REFRESH_INTERVAL_MS);
 const localInterval = setInterval(() => {
-  if (status.value !== "playing") {
+  if (isPaused.value) {
     return;
   }
   currentTime.value = currentTime.value + 50;
